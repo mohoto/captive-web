9ef804604514f2a879b9a696e9f605ad298fc45e
Walid Benmouffok <walidbenmouffok@MacBookPro.lan>
2025-09-04 13:31:47 +0200

    rectif href

--- components/front/common/footerTwo.tsx
+++ components/front/common/footerTwo.tsx
@@ -57,7 +57,7 @@ function FooterTwo() {
               </p>
               <button className="group text-shikam-normal border-shikam-normal px-8 py-3 rounded-full border-2 bg-captive-primary hover:bg-green-700 hover:text-white transition-all duration-75 ease-in-out">
                 <Link
-                  href="https://wa.me/33757837110?text=Bonjour,%20je%20vous%20contacte%20pour%20la%20%creation20de%20site%20web"
+                  href="https://wa.me/33757837110?text=Bonjour,%20je%20vous%20contacte%20pour%20la%20creation%20de%20site%20web"
                   className="flex gap-2 items-center text-lg font-semibold text-blue-950 group-hover:text-white"
                 >
                   <RiWhatsappFill className="h-8 w-8 text-green-700 group-hover:text-white" />
